b82e22f9066c3e30e436e91e863646343771ce1b
Martin <martinpi721@gmail.com>
2024-01-13 10:52:21 +0000

    Added test to obfuscate a packet body

--- tests/test_packet.py
+++ tests/test_packet.py
@@ -38,3 +38,17 @@ class TestTACACSPlusPacket:
 
         assert pkt.deobfuscate == b'\x01\x00\x02\x01\x04\x0b\r\x04testpython_tty0python_de\xef"\x8a\xbd\xbe\xa5\x85\x0e'
 
+
+    def test_obfuscation_of_packet_body(self):
+        """Test we can unobfuscate packet bodies"""
+
+        raw_body = b'\x01\x00\x02\x01\x04\x0b\r\x04testpython_tty0python_de\xef"\x8a\xbd\xbe\xa5\x85\x0e'
+
+        # Configure the header
+        header = Header({'version': 193, 'packet_type': flags.TAC_PLUS_AUTHEN,
+                         'session_id': 1932205026, 'length': 40})
+
+        raw = six.BytesIO(raw_body)
+        pkt = Packet(header, raw.read(), 'test')
+
+        assert pkt.obfuscate == b'\x02\xe5\xad}\x989\xa7\x97\xba\x93\xbbWV\xa1\xad\xa4\xab,\xf9\x9a.g5Z5\xb1\xb2\xb5\xa7q\x92\xf2\n\x13\xd3\xf0\xe3\x9e\xc9\x19'
