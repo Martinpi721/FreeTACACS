b4ca8d693d96440c0142c3592e874cb460aebc9c
Martin <martinpi721@gmail.com>
2024-01-21 22:09:39 +0000

    Added comments

--- freetacacs_plugin.py
+++ freetacacs_plugin.py
@@ -7,9 +7,10 @@ from twisted.internet.endpoints import serverFromString
 
 # Update path to get twistd to import modules corretly
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+# Local imports
 from freetacacs.factory import ITACACSPlusFactory
 from freetacacs.service import TACACSPlusService
-
 
 # Startup the app
 application = service.Application("freetacacs", uid=1, gid=1)
