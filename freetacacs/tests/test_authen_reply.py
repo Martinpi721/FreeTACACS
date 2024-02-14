1674a93e752920513fe5a9b2e4b37795cb17db67
Martin <martinpi721@gmail.com>
2024-02-14 08:36:22 +0000

    Split out test of auth reply data class into its own class

--- freetacacs/tests/test_authen_reply.py
+++ freetacacs/tests/test_authen_reply.py
@@ -18,8 +18,85 @@ from freetacacs.header import TACACSPlusHeader as Header
 from freetacacs.authentication import AuthenReplyFields
 from freetacacs.authentication import TACACSPlusAuthenReply as AuthenReplyPacket
 
+class TestAuthenReplyFields:
+    """Test class for testing the Authentication Reply Fields class"""
+
+
+    def test_invalid_status(self):
+        """Test we handle passing a invalid status field type"""
+
+        version = 193
+        packet_type = flags.TAC_PLUS_AUTHEN
+        session_id = 2620865572
+        length = 40
+
+        # Configure the header
+        header = Header(HeaderFields(version, packet_type, session_id, length))
+
+        with pytest.raises(TypeError) as e:
+            fields = AuthenReplyFields(status='invalid', flags=0x00,
+                                       server_msg='test', data='test')
+
+        assert str(e.value) == 'Status should be of type int'
+
+
+    def test_invalid_flags(self):
+        """Test we handle passing a invalid flags field type"""
+
+        version = 193
+        packet_type = flags.TAC_PLUS_AUTHEN
+        session_id = 2620865572
+        length = 40
+
+        # Configure the header
+        header = Header(HeaderFields(version, packet_type, session_id, length))
+
+        with pytest.raises(TypeError) as e:
+            fields = AuthenReplyFields(status=0, flags='invalid',
+                                       server_msg='test', data='test')
+
+        assert str(e.value) == 'Flags should be of type int'
+
+
+    def test_invalid_server_msg(self):
+        """Test we handle passing a invalid server_msg field type"""
+
+        version = 193
+        packet_type = flags.TAC_PLUS_AUTHEN
+        session_id = 2620865572
+        length = 40
+
+        # Configure the header
+        header = Header(HeaderFields(version, packet_type, session_id, length))
+
+        with pytest.raises(TypeError) as e:
+            fields = AuthenReplyFields(status=0x00, flags=0x00,
+                                      server_msg=0, data='test')
+
+        assert str(e.value) == 'Server Message should be of type string'
+
+
+    def test_invalid_data(self):
+        """Test we handle passing a invalid data field type"""
+
+        version = 193
+        packet_type = flags.TAC_PLUS_AUTHEN
+        session_id = 2620865572
+        length = 40
+
+        # Configure the header
+        header = Header(HeaderFields(version, packet_type, session_id, length))
+
+        with pytest.raises(TypeError) as e:
+            fields = AuthenReplyFields(status=0x00, flags=0x00,
+                                      server_msg='test', data=0)
+
+        assert str(e.value) == 'Data should be of type string'
+
+
 class TestAuthenReply:
     """Test class for testing the Authentication Reply class"""
+
 
     def test_create_instance_without_body_nor_fields(self):
         """Test we handle failure to pass either body for fields"""
@@ -60,82 +137,6 @@ class TestAuthenReply:
         assert isinstance(pkt, AuthenReplyPacket)
         assert str(pkt) == 'status: None, flags: None, server_msg_len: None,' \
                            ' data_len: None, server_msg: None, data: None'
-
-
-    def test_invalid_status(self):
-        """Test we handle passing a invalid status field type"""
-
-        version = 193
-        packet_type = flags.TAC_PLUS_AUTHEN
-        session_id = 2620865572
-        length = 40
-
-        # Configure the header
-        header = Header(HeaderFields(version, packet_type, session_id, length))
-
-        with pytest.raises(TypeError) as e:
-            fields = AuthenReplyFields(status='invalid', flags=0x00,
-                                       server_msg='test', data='test')
-            AuthenReplyPacket(header, fields=fields, secret='test')
-
-        assert str(e.value) == 'Status should be of type int'
-
-
-    def test_invalid_flags(self):
-        """Test we handle passing a invalid flags field type"""
-
-        version = 193
-        packet_type = flags.TAC_PLUS_AUTHEN
-        session_id = 2620865572
-        length = 40
-
-        # Configure the header
-        header = Header(HeaderFields(version, packet_type, session_id, length))
-
-        with pytest.raises(TypeError) as e:
-            fields = AuthenReplyFields(status=0, flags='invalid',
-                                       server_msg='test', data='test')
-            AuthenReplyPacket(header, fields=fields, secret='test')
-
-        assert str(e.value) == 'Flags should be of type int'
-
-
-    def test_invalid_server_msg(self):
-        """Test we handle passing a invalid server_msg field type"""
-
-        version = 193
-        packet_type = flags.TAC_PLUS_AUTHEN
-        session_id = 2620865572
-        length = 40
-
-        # Configure the header
-        header = Header(HeaderFields(version, packet_type, session_id, length))
-
-        with pytest.raises(TypeError) as e:
-            fields = AuthenReplyFields(status=0x00, flags=0x00,
-                                      server_msg=0, data='test')
-            AuthenReplyPacket(header, fields=fields, secret='test')
-
-        assert str(e.value) == 'Server Message should be of type string'
-
-
-    def test_invalid_data(self):
-        """Test we handle passing a invalid data field type"""
-
-        version = 193
-        packet_type = flags.TAC_PLUS_AUTHEN
-        session_id = 2620865572
-        length = 40
-
-        # Configure the header
-        header = Header(HeaderFields(version, packet_type, session_id, length))
-
-        with pytest.raises(TypeError) as e:
-            fields = AuthenReplyFields(status=0x00, flags=0x00,
-                                      server_msg='test', data=0)
-            AuthenReplyPacket(header, fields=fields, secret='test')
-
-        assert str(e.value) == 'Data should be of type string'
 
 
     def test_create_instance_with_fields(self):
